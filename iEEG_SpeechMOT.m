afdc525d1ae1f9f5de4ef6c538c012e826c31657
brunomesqu <92873888+brunomesqu@users.noreply.github.com>
2022-12-15 12:25:57 -0500

    Updated info

--- iEEG_SpeechMOT.m
+++ iEEG_SpeechMOT.m
@@ -1,8 +1,8 @@
 function targdist_fmri_final
 cd('C:\Users\Axolotl\Desktop\iEEG')
 %Screen('Preference','SkipSyncTests', 1); %keep during debugging
-%% written by Jason Rajsic, March 2011; Harrison Ritz, March 2015 (harrison.ritz@gmail.com)
-%% Program requires participants to track 2:6 objects while listening to speech
+%% written by Jason Rajsic, March 2011; Harrison Ritz, March 2015 (harrison.ritz@gmail.com); Bruno Mesquita, December 2022
+%% Program requires participants to track 1:4 objects while listening to speech
 
 
 
